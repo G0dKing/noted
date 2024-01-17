cea8be576ed9d495f4597f2279c01607180daa61
G0dking <ajcaskey@gmail.com>
2024-01-17 07:17:13 -0500

    Built Login Modal

--- client/src/assets/LandingPage.jsx
+++ client/src/assets/LandingPage.jsx
@@ -3,47 +3,37 @@
 import React, { useState } from 'react';
 import logo from './logo.png';
 import bgVideo from './bg.mp4';
-import BtnMain from './BtnMain'
+import BtnMain from './BtnMain';
 import Copyright from './Copyright';
 import Login from './Login';
 import '../css/LandingPage.css';
 import '../css/fonts.css';
 
 const LandingPage = () => {
-
   const [isModalVisible, setIsModalVisible] = useState(false);
-
   const toggleModal = () => {
     console.log("toggleModal called");
     setIsModalVisible(!isModalVisible);
-
   };
 
   return (
     <div className="container">
-
       <video autoPlay muted loop className="bgVid">
         <source src={bgVideo} type="video/mp4" />
       </video>
-
       <header className="header">
-
         <div className="hero">
+
           <img src={logo} className="logo" alt="" />
           <div className="btn-container">
             <BtnMain onClick={toggleModal} />
-            <Login isVisible={isModalVisible} onClose={toggleModal}>
-              <p>Login Form Goes Here</p>
-            </Login>
+            <Login isVisible={isModalVisible} onClose={toggleModal} />
           </div>
         </div>
 
       </header>
-
       <Copyright />
-
     </div>
   );
 };
-
 export default LandingPage;
--- client/src/assets/Login.jsx
+++ client/src/assets/Login.jsx
@@ -1,8 +1,10 @@
-// lLogin.jsx
+// Login.jsx
 
 import React from 'react';
 import '../css/Login.css';
 import BtnClose from './BtnClose';
+import google from './google.svg';
+import fb from './fb.svg';
 
 const Login = ({ isVisible, onClose, children }) => {
     if (!isVisible) return null;
@@ -12,8 +14,24 @@ const Login = ({ isVisible, onClose, children }) => {
             <div className='modal'>
                 {children}
                 <BtnClose onClick={onClose} />
-            </div>
-        </div>
+                <h2>Login</h2>
+                <div className='text-username'>
+                    <textarea placeholder='Username'></textarea>
+                </div>
+                <div className='text-password'>
+                    <textarea placeholder='Password'></textarea>
+                </div>
+                <button className='btn-submit'>Submit</button>
+                <div className='login-socials'>
+                    <button className='google'><img src={google}></img></button>
+                    <button className='fb'><img src={fb}></img></button>
+
+                </div>
+                <div className='modal-footer'>
+                    <p>No Account? <a href="">Register</a></p>
+                </div>
+            </div >
+        </div >
     );
 };
 

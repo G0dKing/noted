ce06e1e3923ba86ded10bafa3c67b280db326122
G0dking <ajcaskey@gmail.com>
2024-01-19 17:38:02 -0500

    Debugging Auth Logic

--- client/src/assets/Login.jsx
+++ client/src/assets/Login.jsx
@@ -1,74 +1,85 @@
-import React, { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
-import BtnClose from './BtnClose';
-import google from './google.svg';
-import fb from './fb.svg';
-import '../css/Login.css';
+// Login.jsx
+
+import React, { useState } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
+import BtnClose from './BtnClose'
+import google from './google.svg'
+import fb from './fb.svg'
+import '../css/Login.css'
+
 
 const Login = ({ isVisible, toggleModal }) => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const navigate = useNavigate();
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const navigate = useNavigate()
 
-    const handleLogin = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await fetch('/api/auth/login', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ username, password })
-            });
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            const data = await response.json();
-            if (data.token) {
-                localStorage.setItem('token', data.token);
-                toggleModal();
-                navigate('/dashboard');
-            }
-        } catch (error) {
-            console.error('Error during login:', error);
-            // Handle error appropriately
-        }
-    };
+  const handleLogin = async e => {
+    e.preventDefault()
+    try {
+      const response = await fetch('/api/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password })
+      })
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`)
+      }
+      const data = await response.json()
+      if (data.token) {
+        localStorage.setItem('token', data.token)
+        toggleModal()
+        navigate('/dashboard')
+      }
+    } catch (error) {
+      console.error('Error during login:', error)
+      // Handle error appropriately
+    }
+  }
 
-    if (!isVisible) return null;
+  if (!isVisible) return null
 
-    return (
-        <div className='modal-backdrop'>
-            <div className='modal'>
-                <BtnClose onClick={toggleModal} />
-                <h2>Login</h2>
-                <form onSubmit={handleLogin}>
-                    <div className='text-username'>
-                        <input
-                            type='text'
-                            placeholder='Username'
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                        />
-                    </div>
-                    <div className='text-password'>
-                        <input
-                            type='password'
-                            placeholder='Password'
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </div>
-                    <button type='submit' className='btn-submit'>Submit</button>
-                </form>
-                <div className='login-socials'>
-                    <button className='google'><img src={google} alt='Google'></img></button>
-                    <button className='fb'><img src={fb} alt='Facebook'></img></button>
-                </div>
-                <div className='modal-footer'>
-                    <p>No Account? <Link to="/register">Register</Link></p>
-                </div>
-            </div>
+  return (
+    <div className='modal-backdrop'>
+      <div className='modal'>
+        <BtnClose onClick={toggleModal} />
+        <h2>Login</h2>
+        <form onSubmit={handleLogin}>
+          <div className='text-username'>
+            <input
+              type='text'
+              placeholder='Username'
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+            />
+          </div>
+          <div className='text-password'>
+            <input
+              type='password'
+              placeholder='Password'
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+          </div>
+          <button type='submit' className='btn-submit'>
+            Submit
+          </button>
+        </form>
+        <div className='login-socials'>
+          <button className='google'>
+            <img src={google} alt='Google'></img>
+          </button>
+          <button className='fb'>
+            <img src={fb} alt='Facebook'></img>
+          </button>
         </div>
-    );
-};
+        <div className='modal-footer'>
+          <p>
+            No Account? <Link to='/register'>Register</Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
 
-export default Login;
+export default Login
--- server.js
+++ server.js
@@ -1,33 +1,39 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const app = express();
-const mongoose = require('mongoose');
-const authRoutes = require('./routes/auth');
-const PORT = process.env.PORT || 3001;
+// server.js
 
-app.use(cors());
+const express = require('express')
+const router = express.Router
+const cors = require('cors')
+const User = require('./models/user')
+const jwt = require('jsonwebtoken')
+const path = require('path')
+const app = express()
+const mongoose = require('mongoose')
+const authRoutes = require('./routes/auth')
+const PORT = process.env.PORT || 3001
+
+app.use(cors())
 
 app.get('/api/data', cors(), (req, res) => {
-    // Your code here
-});
-
-app.use('/api/auth', authRoutes);
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/noted', {
-  // MongoDB options
+  // Your code here
 })
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log(err));
 
-app.use(express.static(path.join(__dirname, './client/dist')));
+app.use('/api/auth', authRoutes)
 
-const indexPath = path.join(__dirname, './client/dist/index.html');
+mongoose
+  .connect(process.env.MONGODB_URI || 'mongodb://localhost:27017', {
+    // MongoDB options
+  })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err))
+
+app.use(express.static(path.join(__dirname, './client/dist')))
+
+const indexPath = path.join(__dirname, './client/dist/index.html')
 
 app.get('*', (req, res) => {
-  res.sendFile(indexPath);
-});
+  res.sendFile(indexPath)
+})
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+  console.log(`Server is listening on port ${PORT}`)
+})

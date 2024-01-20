363118f16cf9a3b49167d40e81e374ef74491bab
G0dking <ajcaskey@gmail.com>
2024-01-20 00:35:00 -0500

    Auth

---
+++ client/src/assets/Dashboard.jsx
@@ -0,0 +1,16 @@
+// Dashboard.jsx
+
+import '../css/Dashboard.css'
+
+const Dashboard = () => {
+  return (
+    <div className='dash-container'>
+      <div className='dash-header'>
+        <h1>DASHBOARD</h1>
+        <h2>(Coming Soon)</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
--- client/src/assets/Login.jsx
+++ client/src/assets/Login.jsx
@@ -13,8 +13,28 @@ const Login = ({ isVisible, toggleModal }) => {
 
   const handleLogin = async (e) => {
     e.preventDefault();
-     navigate('/dashboard');
-  };
+     try {
+        const response = await fetch('/api/auth/login', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ username, password })
+        });
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        if (data.token) {
+          localStorage.setItem('token', data.token);
+          toggleModal();
+          navigate('/dashboard'); // Navigate to dashboard
+        } else {
+          // Handle failed login
+        }
+      } catch (error) {
+        console.error('Error during login:', error);
+        // Handle error appropriately
+  }
+};
 
   if (!isVisible) return null;
 
